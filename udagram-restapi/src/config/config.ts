0fd2e590f53d9daef7f5c88bbdb8ee54fa6dd645
shaishav <shaishav.shah.cs@gmail.com>
2020-06-17 23:59:52 -0400

    udagram-restapi - Initial code

---
+++ udagram-restapi/src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": process.env.UdagramPostgresqlUsername,
+    "password": process.env.UdagramPostgresqlPassword,
+    "database": process.env.UdagramPostgresqlDatabase,
+    "host": process.env.UdagramPostgresqlHost,
+    "dialect": process.env.UdagramPostgresqlDialect,
+    "aws_region": process.env.UdagramAWSRegion,
+    "aws_profile": process.env.UdagramAWSProfile,
+    "aws_media_bucket": process.env.UdagramAWSMediaBucket
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  },
+  "jwt": {
+    "secret": "mysecret"
+  }
+}
